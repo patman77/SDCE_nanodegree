797ce55379998118cb2f1d8406ba3e4f8cc8ce2b
patman77 <info@patman-industries.com>
2018-08-19 19:46:06 +0200

    clone.py works, cv2 paths do not have to start with "../" (arrgh)

--- Term1/Project3_Behavioral_Cloning_Project/clone.py
+++ Term1/Project3_Behavioral_Cloning_Project/clone.py
@@ -1,9 +1,11 @@
 import csv
 import cv2
 import numpy as np
+#import keras
 
 lines = []
-with open('../../data/driving_log.csv') as csvfile:
+#with open('./data/driving_log_stripped.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         print(line)
@@ -12,22 +14,27 @@ with open('../../data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-    source_path =line[0]
+    source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    current_path = './data/IMG/' + filename
+    #print("current_path ", filename)
     image =cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-X_valid = np.array(measurements)
+imagesasarray = np.asarray(images)
+
+X_train = np.array(imagesasarray)
+print('shape X_train: ', X_train.shape)
+y_train = np.array(measurements)
+print('shape y_train: ', y_train.shape)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')

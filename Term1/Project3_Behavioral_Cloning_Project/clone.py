3c76cd6f3a65f3805c860070bd237600e2623119
patman77 <info@patman-industries.com>
2018-08-21 01:25:17 +0200

    replaced simple neural net by LeNet, according to lesson 11. More Networks

--- Term1/Project3_Behavioral_Cloning_Project/clone.py
+++ Term1/Project3_Behavioral_Cloning_Project/clone.py
@@ -37,11 +37,24 @@ print('shape y_train: ', y_train.shape)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Flatten(input_shape=(160, 320, 3)))
-model.add(Dense(1))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+if False:
+    #model.add(Flatten(input_shape=(160, 320, 3)))
+    model.add(Flatten())
+    model.add(Dense(1))
+else:
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)

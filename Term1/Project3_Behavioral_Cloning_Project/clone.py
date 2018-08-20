b1d6d3072cf78a7d75078eed12319427dd549bb4
patman77 <info@patman-industries.com>
2018-08-20 20:34:48 +0200

    added keras lambda layer as in lecture 10. Data Preprocessing

--- Term1/Project3_Behavioral_Cloning_Project/clone.py
+++ Term1/Project3_Behavioral_Cloning_Project/clone.py
@@ -38,6 +38,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 

163a99a7d7e7e7f2b15e64ab9b64e8f8a311a15f
patman77 <info@patman-industries.com>
2018-08-23 00:47:03 +0200

    added NVIDIA model from the lecture

--- Term1/Project3_Behavioral_Cloning_Project/clone.py
+++ Term1/Project3_Behavioral_Cloning_Project/clone.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 #import keras
+from enum import Enum
 
 lines = []
 #with open('./data/driving_log.csv') as csvfile:
@@ -42,11 +43,23 @@ from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-if False:
+
+class NetType(Enum):
+    SIMPLE = 1
+    LeNet = 2
+    NVIDIA = 3
+
+#nettype = NetType.SIMPLE
+#nettype = NetType.LeNet
+nettype = NetType.NVIDIA
+
+if NetType.SIMPLE == nettype:
+    print("simple model")
     #model.add(Flatten(input_shape=(160, 320, 3)))
     model.add(Flatten())
     model.add(Dense(1))
-else:
+elif NetType.LeNet == nettype:
+    print("LeNet model")
     model.add(Convolution2D(6,5,5,activation="relu"))
     model.add(MaxPooling2D())
     model.add(Convolution2D(6,5,5,activation="relu"))
@@ -55,6 +68,20 @@ else:
     model.add(Dense(120))
     model.add(Dense(84))
     model.add(Dense(1))
+elif NetType.NVIDIA == nettype:
+    print("NVIDIA model")
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,3,3, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,3,3, subsample=(2,2), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)

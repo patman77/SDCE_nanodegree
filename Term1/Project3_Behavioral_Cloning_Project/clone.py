ab385131755aca32aaecd990fbd3cf42edebc631
patman77 <info@patman-industries.com>
2018-08-23 17:31:22 +0200

    increased number of epochs from 7 to 20

--- Term1/Project3_Behavioral_Cloning_Project/clone.py
+++ Term1/Project3_Behavioral_Cloning_Project/clone.py
@@ -85,7 +85,7 @@ elif NetType.NVIDIA == nettype:
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=20)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=20)
 
 model.save('model.h5')

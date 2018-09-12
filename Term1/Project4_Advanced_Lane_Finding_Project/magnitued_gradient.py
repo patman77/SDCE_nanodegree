4fd2ceaba7cbbb6fb06e71f55691e6166b999bbf
Patrick Klie <p@dunreeb.com>
2018-09-13 00:28:46 +0200

    * added further gradient stuff from the course

---
+++ Term1/Project4_Advanced_Lane_Finding_Project/magnitued_gradient.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+import math
+
+
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
+# Define a function that applies Sobel x and y, 
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude
+    abs_sobelxy = np.sqrt(sobelx**2 + sobely**2)
+#    abs_sobelx = np.absolute(sobelx)
+#    abs_sobely = np.absolute(sobely)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+#    scaled_sobelx = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+#    scaled_sobely = np.uint8(255*abs_sobely/np.max(abs_sobely))
+    scaled_sobelxy = np.uint8(255*abs_sobelxy/np.max(abs_sobelxy))
+    # 5) Create a binary mask where mag thresholds are met
+    sxbinary = np.zeros_like(scaled_sobelxy)
+    sxbinary[(scaled_sobelxy >= mag_thresh[0]) & (scaled_sobelxy <= mag_thresh[1])] = 1
+
+    # 6) Return this mask as your binary_output image
+    #binary_output = np.copy(img) # Remove this line
+    #return binary_output
+    return sxbinary
+    
+# Run the function
+mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(mag_binary, cmap='gray')
+ax2.set_title('Thresholded Magnitude', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)

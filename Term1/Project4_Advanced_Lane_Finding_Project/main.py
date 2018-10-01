4ccf7e01dfd3a4a5e1eb8cf858695dbf3a9f6305
patman77 <info@patman-industries.com>
2018-10-01 18:48:01 +0200

    added backprojection of measurement from section: "Tips and Tricks for the Project"

--- Term1/Project4_Advanced_Lane_Finding_Project/main.py
+++ Term1/Project4_Advanced_Lane_Finding_Project/main.py
@@ -180,3 +180,31 @@ print(" ")
 # step 7: Warp the detected lane boundaries back onto the original image.
 
 # step 8: Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
+
+
+def backproject_measurement(warped, ploty, left_fitx, right_fitx):
+    """ projects measurements back down onto the road
+    :param warped    : warped binary image
+    :param ploty     : lane line pixels, y-range
+    :param left_fitx : x pixel values of the fitted lines
+    :param right_fitx: y pixel values of the fitted lines
+    :return:
+    """
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    plt.imshow(result)
+    #pass

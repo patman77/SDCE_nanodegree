63d4445a3ac210e8c6d53d996282ec6a9f749cfd
Patrick Klie <info@patman-industries.com>
2018-10-02 01:04:18 +0200

    added curvature calculaion with real lane data, not just fake data

--- Term1/Project4_Advanced_Lane_Finding_Project/radius_curve.py
+++ Term1/Project4_Advanced_Lane_Finding_Project/radius_curve.py
@@ -54,6 +54,32 @@ def measure_curvature_real():
     return left_curverad, right_curverad
 
 
+def measure_curvature_real2(ploty, left_fitx, right_fitx):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
+    # Start by generating our fake example data
+    # Make sure to feed in your real data instead in your project!
+    #ploty, left_fit_cr, right_fit_cr = generate_data(ym_per_pix, xm_per_pix)
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, left_fitx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, right_fitx*xm_per_pix, 2)
+
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+
+    ##### TO-DO: Implement the calculation of R_curve (radius of curvature) #####
+    left_curverad = ((1 + ((2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2)) ** 1.5) / np.abs(
+        2 * left_fit_cr[0])  ## Implement the calculation of the left line here
+    right_curverad = ((1 + ((2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2)) ** 1.5) / np.abs(
+        2 * right_fit_cr[0])  ## Implement the calculation of the right line here
+
+    return left_curverad, right_curverad
+
 # Calculate the radius of curvature in meters for both lane lines
 #left_curverad, right_curverad = measure_curvature_real()
 

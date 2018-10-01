ff8be7eb55f559d1216ebe6194f361bef8093b77
Patrick Klie <info@patman-industries.com>
2018-10-02 00:25:14 +0200

    added fit_polynomial2 giving out lane data for furhter processing

--- Term1/Project4_Advanced_Lane_Finding_Project/sliding_window.py
+++ Term1/Project4_Advanced_Lane_Finding_Project/sliding_window.py
@@ -120,6 +120,35 @@ def fit_polynomial(binary_warped):
 
     return out_img
 
+def fit_polynomial2(binary_warped):
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+
+    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+    left_fit = np.polyfit(lefty,leftx,2)
+    right_fit = np.polyfit(righty,rightx,2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    try:
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    except TypeError:
+        # Avoids an error if `left` and `right_fit` are still none or incorrect
+        print('The function failed to fit a line!')
+        left_fitx = 1*ploty**2 + 1*ploty
+        right_fitx = 1*ploty**2 + 1*ploty
+
+    ## Visualization ##
+    # Colors in the left and right lane regions
+    out_img[lefty, leftx] = [255, 0, 0]
+    out_img[righty, rightx] = [0, 0, 255]
+
+    # Plots the left and right polynomials on the lane lines
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+
+    return out_img, ploty, left_fitx, right_fitx
 # # Load our image
 # binary_warped = mpimg.imread('warped_example_gray.jpg')
 #

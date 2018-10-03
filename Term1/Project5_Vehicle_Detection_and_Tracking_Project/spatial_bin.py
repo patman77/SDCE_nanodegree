813af88303ed94179c804577c9db4daba8d4afc6
Patrick Klie <info@patman-industries.com>
2018-10-04 01:10:10 +0200

    added spatial_bin.py from lecture

---
+++ Term1/Project5_Vehicle_Detection_and_Tracking_Project/spatial_bin.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read in an image
+# You can also read cutout2, 3, 4 etc. to see other examples
+image = mpimg.imread('cutout1.jpg')
+
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+# KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH
+# cv2.imread() INSTEAD YOU START WITH BGR COLOR!
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if "HSV" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif "Luv" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2Luv)
+        elif "XYZ" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)
+        elif "GRAY" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        elif "YCrCb" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        elif "Lab" == color_space:
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
+
+    else:
+        feature_image = np.copy(img)
+
+    # Use cv2.resize().ravel() to create the feature vector
+    # features = img.ravel() # Remove this line!
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vector
+    return features
+
+
+feature_vec = bin_spatial(image, color_space='Lab', size=(32, 32))
+
+# Plot features
+plt.plot(feature_vec)
+plt.title('Spatially Binned Features')

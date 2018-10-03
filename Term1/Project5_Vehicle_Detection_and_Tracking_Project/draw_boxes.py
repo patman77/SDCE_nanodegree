b70fdd24b485d1e7c9be5f219eb84998edb7d13f
Patrick Klie <info@patman-industries.com>
2018-10-04 01:05:08 +0200

    corrected draw_boxes.py

--- Term1/Project5_Vehicle_Detection_and_Tracking_Project/draw_boxes.py
+++ Term1/Project5_Vehicle_Detection_and_Tracking_Project/draw_boxes.py
@@ -1,6 +1,12 @@
-# Define a function that takes an image, a list of bounding boxes, 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+# Define a function that takes an image, a list of bounding boxes,
 # and optional color tuple and line thickness as inputs
 # then draws boxes in that color on the output
+
+image = mpimg.imread('bbox-example-image.jpg')
 
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
@@ -16,3 +22,6 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
 bboxes = [((275, 572), (380, 510)), ((488, 563), (549, 518)), ((554, 543), (582, 522)), 
           ((601, 555), (646, 522)), ((657, 545), (685, 517)), ((849, 678), (1135, 512))]
 
+result = draw_boxes(image, bboxes)
+plt.imshow(result)
+print('end')

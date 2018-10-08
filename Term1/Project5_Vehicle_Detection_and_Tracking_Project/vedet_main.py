fb7324741d5be5e2d79566116230ae4be2375060
Patrick Klie <p@dunreeb.com>
2018-10-09 00:47:30 +0200

    started loading of pretrained model

--- Term1/Project5_Vehicle_Detection_and_Tracking_Project/vedet_main.py
+++ Term1/Project5_Vehicle_Detection_and_Tracking_Project/vedet_main.py
@@ -101,24 +101,6 @@ def process_image_vedet(image):
     return draw_img
 
 modelfilename = "model.svc"
-
-# Read in cars and notcars
-carimages = glob.glob('./vehicles/**/*.png', recursive=True)
-notcarimages = glob.glob('./non-vehicles/**/*.png', recursive=True)
-cars = []
-notcars = []
-for image in carimages:
-    cars.append(image)
-for image in notcarimages:
-    notcars.append(image)
-
-# Reduce the sample size because
-# The quiz evaluator times out after 13s of CPU time
-sample_size = min(len(cars), len(notcars))
-print('#cars = ', len(cars), '#notcars = ', len(notcars), 'sample_size = ', sample_size)
-cars = cars[0:sample_size]
-notcars = notcars[0:sample_size]
-
 ### TODO: Tweak these parameters and see how the results change.
 color_space = 'HSV'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 18  # HOG orientations
@@ -132,55 +114,138 @@ hist_feat = True  # Histogram features on or off
 hog_feat = True  # HOG features on or off
 y_start_stop = [400, 707]  # Min and max in y to search in slide_window()
 
-car_features = extract_features(cars, color_space=color_space,
-                                spatial_size=spatial_size, hist_bins=hist_bins,
-                                orient=orient, pix_per_cell=pix_per_cell,
-                                cell_per_block=cell_per_block,
-                                hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                hist_feat=hist_feat, hog_feat=hog_feat)
-notcar_features = extract_features(notcars, color_space=color_space,
-                                   spatial_size=spatial_size, hist_bins=hist_bins,
-                                   orient=orient, pix_per_cell=pix_per_cell,
-                                   cell_per_block=cell_per_block,
-                                   hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                   hist_feat=hist_feat, hog_feat=hog_feat)
+# check if previously saved model exists, then use it
+if os.path.isfile(modelfilename):
+    with open(modelfilename, mode="rb") as f:
+        data = pickle.load(f)
+    if "svc" in data:
+        svc = data["svc"]
+        print("Loaded linear SVC classifier:")
+        car_features = data["cars_features"]
+        notcar_features = data["notcars_features"]
+        colorspace = data["colorspace"]
+        orient = data["orient"]
+        pix_per_cell = data["pix_per_cell"]
+        cell_per_block = data["cell_per_block"]
+        hog_channel = data["hog_channel"]
+        spatial_size = data["spatial_size"]
+        hist_bins = data["hist_bins"]
+        spatial_feat = data["spatial_feat"]
+        hist_feat = data["hist_feat"]
+        hog_feat = data["hog_feat"]
+    else:
+        svc = None
+        print("Loaded linear SVC classifier:")
+
+    print("Loaded model!")
+else:
+    # Read in cars and notcars
+    carimages = glob.glob('./vehicles/**/*.png', recursive=True)
+    notcarimages = glob.glob('./non-vehicles/**/*.png', recursive=True)
+    cars = []
+    notcars = []
+    for image in carimages:
+        cars.append(image)
+    for image in notcarimages:
+        notcars.append(image)
+
+    # Reduce the sample size because
+    # The quiz evaluator times out after 13s of CPU time
+    sample_size = min(len(cars), len(notcars))
+    print('#cars = ', len(cars), '#notcars = ', len(notcars), 'sample_size = ', sample_size)
+    cars = cars[0:sample_size]
+    notcars = notcars[0:sample_size]
+
+    car_features = extract_features(cars, color_space=color_space,
+                                    spatial_size=spatial_size, hist_bins=hist_bins,
+                                    orient=orient, pix_per_cell=pix_per_cell,
+                                    cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                    hist_feat=hist_feat, hog_feat=hog_feat)
+    notcar_features = extract_features(notcars, color_space=color_space,
+                                       spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                       hist_feat=hist_feat, hog_feat=hog_feat)
+
+    # Use a linear SVC
+    svc = LinearSVC()
+
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=rand_state)
+
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X_train)
+    # Apply the scaler to X
+    X_train = X_scaler.transform(X_train)
+    X_test = X_scaler.transform(X_test)
+
+    print('Using:', orient, 'orientations', pix_per_cell,
+          'pixels per cell and', cell_per_block, 'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+
+
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t = time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these', n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
+    data = {
+        "cars_features": car_features,
+        "notcars_features": notcar_features,
+        "colorspace": color_space,
+        "orient": orient,
+        "pix_per_cell": pix_per_cell,
+        "cell_per_block": cell_per_block,
+        "hog_channel": hog_channel,
+        "spatial_size": spatial_size,
+        "hist_bins": hist_bins,
+        "spatial_feat": spatial_feat,
+        "hist_feat": hist_feat,
+        "hog_feat": hog_feat
+    }
+    # Save the training and test features:
+    with open(modelfilename, mode="wb") as f:
+        pickle.dump(data, f)
+
+    data = {
+        "cars_features": car_features,
+        "notcars_features": notcar_features,
+        "colorspace": color_space,
+        "orient": orient,
+        "pix_per_cell": pix_per_cell,
+        "cell_per_block": cell_per_block,
+        "hog_channel": hog_channel,
+        "spatial_size": spatial_size,
+        "hist_bins": hist_bins,
+        "spatial_feat": spatial_feat,
+        "hist_feat": hist_feat,
+        "hog_feat": hog_feat
+    }
 
 # Create an array stack of feature vectors
 X = np.vstack((car_features, notcar_features)).astype(np.float64)
-
 # Define the labels vector
 y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=rand_state)
-
-# Fit a per-column scaler
-X_scaler = StandardScaler().fit(X_train)
-# Apply the scaler to X
-X_train = X_scaler.transform(X_train)
-X_test = X_scaler.transform(X_test)
-
-print('Using:', orient, 'orientations', pix_per_cell,
-      'pixels per cell and', cell_per_block, 'cells per block')
-print('Feature vector length:', len(X_train[0]))
-# Use a linear SVC
-svc = LinearSVC()
-# Check the training time for the SVC
-t = time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2 - t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample
-t = time.time()
-n_predict = 10
-print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-print('For these', n_predict, 'labels: ', y_test[0:n_predict])
-t2 = time.time()
-print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
+X_scaler = StandardScaler().fit(X)
 
 #doitonthevideo = True
 doitonthevideo = False

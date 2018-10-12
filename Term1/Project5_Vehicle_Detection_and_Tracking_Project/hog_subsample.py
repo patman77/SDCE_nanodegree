056baea8d58dcfb2b23a40acaad7789e4cc0066b
Patrick Klie <p@dunreeb.com>
2018-10-13 00:06:38 +0200

    added find_cars2 with better size handling

--- Term1/Project5_Vehicle_Detection_and_Tracking_Project/hog_subsample.py
+++ Term1/Project5_Vehicle_Detection_and_Tracking_Project/hog_subsample.py
@@ -4,29 +4,30 @@ import numpy as np
 import pickle
 import cv2
 from lesson_functions2 import *
+#from lesson_functions import *
 
-# load a pe-trained svc model from a serialized (pickle) file
-dist_pickle = pickle.load(open("svc_pickle.p", "rb"))
+# # load a pe-trained svc model from a serialized (pickle) file
+# dist_pickle = pickle.load(open("svc_pickle.p", "rb"))
+#
+# # get attributes of our svc object
+# svc = dist_pickle["svc"]
+# X_scaler = dist_pickle["scaler"]
+# orient = dist_pickle["orient"]
+# pix_per_cell = dist_pickle["pix_per_cell"]
+# cell_per_block = dist_pickle["cell_per_block"]
+# spatial_size = dist_pickle["spatial_size"]
+# hist_bins = dist_pickle["hist_bins"]
 
-# get attributes of our svc object
-svc = dist_pickle["svc"]
-X_scaler = dist_pickle["scaler"]
-orient = dist_pickle["orient"]
-pix_per_cell = dist_pickle["pix_per_cell"]
-cell_per_block = dist_pickle["cell_per_block"]
-spatial_size = dist_pickle["spatial_size"]
-hist_bins = dist_pickle["hist_bins"]
-
-img = mpimg.imread('test_image.jpg')
+#img = mpimg.imread('test_image.jpg')
 
 
 # Define a single function that can extract features using hog sub-sampling and make predictions
 def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
     draw_img = np.copy(img)
-    img = img.astype(np.float32) / 255
+    #img = img.astype(np.float32) / 255 # don't do it twice
 
     img_tosearch = img[ystart:ystop, :, :]
-    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    ctrans_tosearch = convert_color2(img_tosearch, conv='RGB2YCrCb')
     if scale != 1:
         imshape = ctrans_tosearch.shape
         ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
@@ -48,9 +49,11 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
     nysteps = (nyblocks - nblocks_per_window) // cells_per_step + 1
 
     # Compute individual channel HOG features for the entire image
-    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog1 = get_hog_features2(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features2(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features2(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    window_list = []
 
     for xb in range(nxsteps):
         for yb in range(nysteps):
@@ -69,8 +72,8 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
             subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
 
             # Get color features
-            spatial_features = bin_spatial(subimg, size=spatial_size)
-            hist_features = color_hist(subimg, nbins=hist_bins)
+            spatial_features = bin_spatial2(subimg, size=spatial_size)
+            hist_features = color_hist2(subimg, nbins=hist_bins)
 
             # Scale features and make a prediction
             test_features = X_scaler.transform(
@@ -82,11 +85,94 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
                 xbox_left = np.int(xleft * scale)
                 ytop_draw = np.int(ytop * scale)
                 win_draw = np.int(window * scale)
-                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
-                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 1)
+                # cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
+                #               (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 1)
+                # instead of drawing, return bbox in the style of slide_window and search_windows
+                window_list.append(((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
+    #return draw_img
+    return window_list
 
-    return draw_img
+def find_cars2(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+    draw_img = np.copy(img)
+    #img = img.astype(np.float32) / 255 # don't do it twice
 
+    img_tosearch = img[ystart:ystop, :, :]
+    ctrans_tosearch = convert_color2(img_tosearch, conv='RGB2YCrCb')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
+
+    ch1 = ctrans_tosearch[:, :, 0]
+    ch2 = ctrans_tosearch[:, :, 1]
+    ch3 = ctrans_tosearch[:, :, 2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
+    nfeat_per_block = orient * cell_per_block ** 2
+
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step + 1
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step + 1
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features2(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features2(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features2(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    window_list = []
+    features = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            file_features = []
+            ypos = yb * cells_per_step
+            xpos = xb * cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            #hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3)) # doesnt work
+            hog_features.append(hog_feat1)
+            hog_features.append(hog_feat2)
+            hog_features.append(hog_feat3)
+            hog_features = np.ravel(hog_features)
+
+            xleft = xpos * pix_per_cell
+            ytop = ypos * pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+
+            # Get color features
+            spatial_features = bin_spatial2(subimg, size=spatial_size)
+            hist_features = color_hist2(subimg, nbins=hist_bins)
+
+            file_features.append(spatial_features)
+            file_features.append(hist_features)
+            file_features.append(hog_features)
+            file_features = np.concatenate(file_features)
+            test_features = X_scaler.transform(np.array(file_features).reshape(1, -1))
+            # Scale features and make a prediction
+            # this doesn't work
+            # test_features = X_scaler.transform(
+            #     np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
+            test_prediction = svc.predict(test_features)
+
+            if test_prediction == 1:
+                xbox_left = np.int(xleft * scale)
+                ytop_draw = np.int(ytop * scale)
+                win_draw = np.int(window * scale)
+                # cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
+                #               (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 1)
+                # instead of drawing, return bbox in the style of slide_window and search_windows
+                window_list.append(((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
+    #return draw_img
+    return window_list
 
 # ystart = 400
 # ystop = 656

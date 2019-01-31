d017a0a4cbfc0530c2dbbe106e9d1c0edf1e1315
Patrick Klie <info@patman-industries.com>
2019-01-31 22:51:01 +0100

    implemented KalmanFilter update and predict step; tools calcJacobian and calcRMSE

--- Term2/Project1_Extended_Kalman_Filters/src/tools.cpp
+++ Term2/Project1_Extended_Kalman_Filters/src/tools.cpp
@@ -14,6 +14,33 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // TODO: YOUR CODE HERE
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  if(0 == estimations.size())
+      return rmse;
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size())
+      return rmse;
+
+  // TODO: accumulate squared residuals
+  for (int i=0; i < estimations.size(); ++i) {
+    // ... your code here
+    VectorXd tmpvec = estimations[i] - ground_truth[i];
+    VectorXd tmpvec2 = tmpvec.array() * tmpvec.array();
+    rmse = rmse + tmpvec2;
+  }
+
+  // TODO: calculate the mean
+  rmse = rmse / estimations.size();
+  // TODO: calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  // return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +48,28 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  MatrixXd Hj(3,4);
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // TODO: YOUR CODE HERE 
+
+  // check division by zero
+  static float eps = 0.0000001;
+  if( fabs(px)<eps && fabs(py)<eps )
+    return Hj;
+  
+  // compute the Jacobian matrix
+  float px2 = px*px;
+  float py2 = py*py;
+
+  Hj << px/(sqrt(px2+py2)), py/(sqrt(px2+py2)), 0, 0,
+        -py/(px2+py2), px/(px2+py2), 0, 0,
+        py*(vx*py-vy*px)/(pow(px2+py2, 1.5)), px*(vy*px-vx*py)/(pow(px2+py2, 1.5)),
+        px/(sqrt(px2+py2)), py/(sqrt(px2+py2));
+
+  return Hj;
 }

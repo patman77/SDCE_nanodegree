5ff97d1437bdd9e47817cd7a6457dbcea88f6c62
Patrick Klie <info@patman-industries.com>
2019-02-10 18:38:37 +0100

    added initialization of timestamp for first frame

--- Term2/Project1_Extended_Kalman_Filters/src/FusionEKF.cpp
+++ Term2/Project1_Extended_Kalman_Filters/src/FusionEKF.cpp
@@ -68,8 +68,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
   if (!is_initialized_) {
     /**
-     * TODO: Initialize the state ekf_.x_ with the first measurement.
-     * TODO: Create the covariance matrix.
+     * DONE: Initialize the state ekf_.x_ with the first measurement.
+     * DONE: Create the covariance matrix.
      * You'll need to convert radar from polar to cartesian coordinates.
      */
 
@@ -81,6 +81,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
+        previous_timestamp_ = measurement_pack.timestamp_; // get current timestamp
+
         double rho     = measurement_pack.raw_measurements_[0];
         double phi     = measurement_pack.raw_measurements_[1];
         double rho_dot = measurement_pack.raw_measurements_[2];

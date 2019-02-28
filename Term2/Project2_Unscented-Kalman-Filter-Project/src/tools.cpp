31c4ab536da3955f46fe22c6ced10f4d876c0400
Patrick Klie <info@patman-industries.com>
2019-02-28 21:15:41 +0100

    adopted CalculateRMSE from previous project EKF

--- Term2/Project2_Unscented-Kalman-Filter-Project/src/tools.cpp
+++ Term2/Project2_Unscented-Kalman-Filter-Project/src/tools.cpp
@@ -10,6 +10,34 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-   * TODO: Calculate the RMSE here.
+   * DONE: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // DONE: YOUR CODE HERE
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  if(0 == estimations.size())
+      return rmse;
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size())
+      return rmse;
+
+  // DONE: accumulate squared residuals
+  for (int i=0; i < estimations.size(); ++i) {
+    // ... your code here
+    VectorXd tmpvec = estimations[i] - ground_truth[i];
+    VectorXd tmpvec2 = tmpvec.array() * tmpvec.array();
+    rmse = rmse + tmpvec2;
+  }
+
+  // DONE: calculate the mean
+  rmse = rmse / estimations.size();
+  // DONE: calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  // return the result
+  return rmse;
+
 }

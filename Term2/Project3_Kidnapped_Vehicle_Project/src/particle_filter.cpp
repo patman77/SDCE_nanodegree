3f89cfd5e0e67d262ed1d9846baaf6d470c38b5f
Patrick Klie <info@patman-industries.com>
2019-03-29 23:37:36 +0100

    set number of particle filters to 1000

--- Term2/Project3_Kidnapped_Vehicle_Project/src/particle_filter.cpp
+++ Term2/Project3_Kidnapped_Vehicle_Project/src/particle_filter.cpp
@@ -30,7 +30,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 1000;  // DONE: Set the number of particles
 
 }
 
